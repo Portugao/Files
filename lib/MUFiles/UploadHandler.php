b0e2732355c554a882ef17e754e6a6e14595598b
Michael Ueberschaer <kontakt@webdesign-in-bremen.com>
2013-08-19 18:50:20 +0200

    handle of extensions

--- lib/MUFiles/UploadHandler.php
+++ lib/MUFiles/UploadHandler.php
@@ -16,5 +16,46 @@
  */
 class MUFiles_UploadHandler extends MUFiles_Base_UploadHandler
 {
-    // feel free to add your upload handler enhancements here
+    /**
+     * Constructor initialising the supported object types.
+     */
+    public function __construct()
+    {
+        $this->allowedObjectTypes = array('file');
+        $this->imageFileTypes = array('gif', 'jpeg', 'jpg', 'png', 'swf');
+        $this->forbiddenFileTypes = array('cgi', 'pl', 'asp', 'phtml', 'php', 'php3', 'php4', 'php5', 'exe', 'com', 'bat', 'jsp', 'cfm', 'shtml');
+        $this->allowedFileSizes = array('file' => array('uploadFile' => ModUtil::getVar('MUFiles', 'maxSize')));
+    }
+    
+    /**
+     * Determines the allowed file extensions for a given object type.
+     *
+     * @param string $objectType Currently treated entity type.
+     * @param string $fieldName  Name of upload field.
+     * @param string $extension  Input file extension.
+     *
+     * @return array the list of allowed file extensions
+     */
+    protected function isAllowedFileExtension($objectType, $fieldName, $extension)
+    {
+        // determine the allowed extensions
+        $allowedExtensions = array();
+        switch ($objectType) {
+            case 'file':
+                $allowedExtensions = explode(',', ModUtil::getVar('allowedExtension'));
+                break;
+        }
+    
+        if (count($allowedExtensions) > 0) {
+            if (!in_array($extension, $allowedExtensions)) {
+                return false;
+            }
+        }
+    
+        if (in_array($extension, $this->forbiddenFileTypes)) {
+            return false;
+        }
+    
+        return true;
+    }
 }
