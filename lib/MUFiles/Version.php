ecca0e8fe48a47117900e353dd41f17e9c8f4b9e
Michael Ueberschaer <kontakt@webdesign-in-bremen.com>
2013-12-03 15:32:53 +0100

    some typo of version

--- lib/MUFiles/Version.php
+++ lib/MUFiles/Version.php
@@ -16,5 +16,56 @@
  */
 class MUFiles_Version extends MUFiles_Base_Version
 {
-    // custom enhancements can go here
+/**
+ * Version information base class.
+ */
+class MUFiles_Base_Version extends Zikula_AbstractVersion
+{
+    /**
+     * Retrieves meta data information for this application.
+     *
+     * @return array List of meta data.
+     */
+    public function getMetaData()
+    {
+        $meta = array();
+        // the current module version
+        $meta['version']              = '1.0.0';
+        // the displayed name of the module
+        $meta['displayname']          = $this->__('MUFiles');
+        // the module description
+        $meta['description']          = $this->__('MUFiles module to handle files');
+        //! url version of name, should be in lowercase without space
+        $meta['url']                  = $this->__('mufiles');
+        // core requirement
+        $meta['core_min']             = '1.3.5'; // requires minimum 1.3.5
+        $meta['core_max']             = '1.3.6'; // not ready for 1.3.7 yet
+
+        // define special capabilities of this module
+        $meta['capabilities'] = array(
+                          HookUtil::SUBSCRIBER_CAPABLE => array('enabled' => true)
+/*,
+                          HookUtil::PROVIDER_CAPABLE => array('enabled' => true), // TODO: see #15
+                          'authentication' => array('version' => '1.0'),
+                          'profile'        => array('version' => '1.0', 'anotherkey' => 'anothervalue'),
+                          'message'        => array('version' => '1.0', 'anotherkey' => 'anothervalue')
+*/
+        );
+
+        // permission schema
+        $meta['securityschema'] = array(
+            'MUFiles::' => '::',
+            'MUFiles::Ajax' => '::',
+            'MUFiles:ItemListBlock:' => 'Block title::',
+            'MUFiles:ModerationBlock:' => 'Block title::',
+            'MUFiles:Collection:' => 'Collection ID::',
+            'MUFiles:Collection:Collection' => 'Collection ID:Collection ID:',
+            'MUFiles:File:' => 'File ID::',
+            'MUFiles:Collection:File' => 'Collection ID:File ID:',
+        );
+        // DEBUG: permission schema aspect ends
+
+
+        return $meta;
+    }
 }
