cc16e62853e738507d0bb672b61a62360e39938c
Michael Ueberschaer <kontakt@webdesign-in-bremen.com>
2014-05-09 17:55:35 +0200

    Only collections set inFrontend is true

--- lib/MUFiles/Entity/Repository/Collection.php
+++ lib/MUFiles/Entity/Repository/Collection.php
@@ -18,5 +18,30 @@
  */
 class MUFiles_Entity_Repository_Collection extends MUFiles_Entity_Repository_Base_Collection
 {
-    // feel free to add your own methods here, like for example reusable DQL queries
+    /**
+     * Adds default filters as where clauses.
+     *
+     * @param Doctrine\ORM\QueryBuilder $qb         Query builder to be enhanced.
+     * @param array                     $parameters List of determined filter options.
+     *
+     * @return Doctrine\ORM\QueryBuilder Enriched query builder instance.
+     */
+    protected function applyDefaultFilters(QueryBuilder $qb, $parameters = array())
+    {
+        $currentModule = ModUtil::getName();//FormUtil::getPassedValue('module', '', 'GETPOST');
+        $currentLegacyControllerType = FormUtil::getPassedValue('lct', 'user', 'GETPOST');
+        if ($currentLegacyControllerType == 'admin' && $currentModule == 'MUFiles') {
+            return $qb;
+        }
+    
+        if (!in_array('workflowState', array_keys($parameters)) || empty($parameters['workflowState'])) {
+            // per default we show approved collections only
+            $onlineStates = array('approved');
+            $qb->andWhere('tbl.workflowState IN (:onlineStates)')
+               ->setParameter('onlineStates', $onlineStates)
+               ->andWhere('tbl.inFrontend = 1');
+        }
+    
+        return $qb;
+    }
 }
