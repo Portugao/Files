a8dc89455c401831a1ad2dd0fcc89ba3435b7c26
Michael Ueberschaer <info@homepages-mit-zikula.de>
2018-09-29 14:45:37 +0200

    Fixes after generator update

--- MU/FilesModule/Twig/TwigExtension.php
+++ MU/FilesModule/Twig/TwigExtension.php
@@ -19,5 +19,26 @@ use MU\FilesModule\Twig\Base\AbstractTwigExtension;
  */
 class TwigExtension extends AbstractTwigExtension
 {
+    /**
+     * Returns a list of custom Twig functions.
+     *
+     * @return \Twig_SimpleFunction[]
+     */
+    public function getFunctions()
+    {
+        $functions = parent::getFunctions();
+        $functions[] = new \Twig_SimpleFunction('mufilesmodule_specialMenu', [$this, 'getSpecialMenu']);
+        return $functions;
+    }
+    /**
+     * 
+     * @param integer $collectionId
+     * @param integer $fileId
+     * @return mixed|string
+     */
+    public function getSpecialMenu($collectionId, $fileId)
+    {
+        return $this->listHelper->getCollectionMenue($collectionId, $fileId);
+    }
     // feel free to add your own Twig extension methods here
 }
